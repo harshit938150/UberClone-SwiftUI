23ec8e9dbb7f988b0397c235c70ce832d9dc262d
harshit938150 <palacheralaharshit@gmail.com>
2023-06-10 13:04:07 +0530

    Initial Commit

---
+++ UberClone-SwiftUI/UberClone_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberClone_SwiftUIApp.swift
+//  UberClone-SwiftUI
+//
+//  Created by Harshit Bhargava  on 10/06/23.
+//
+
+import SwiftUI
+
+@main
+struct UberClone_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
